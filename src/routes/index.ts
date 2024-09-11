fdeb2c8cd6f2dbee1d1dcea49b200e3a8b0d2391
smile995 <ah.smile995@gmai.com>
2024-09-11 23:18:00 +0600

    file structure is ready with configure files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,34 +1,18 @@
 import { Router } from "express";
-import { StudentRoutes } from "../modules/student/student.route";
-import { UsersRoutes } from "../modules/user/user.route";
-import { AcademicSemesterRoutes } from "../modules/academicSemester/academicSemester.route";
-import { AcademicFacultyRoutes } from "../modules/academicFaculty/routes.academicFaculty";
-import { AcademicDepartmentRouter } from "../modules/academicDepartment/routes.academicdepartment";
 
+
+const data=()=>{
+    console.log("bike rantel ");
+    
+}
 
 export const router = Router();
 const modulesRoute = [
     {
         path: "/students",
-        route: StudentRoutes
+        route: data
     },
-    {
-        path: "/users",
-        route: UsersRoutes
-    },
-    {
-        path: "/academic-semesters",
-        route: AcademicSemesterRoutes
-    },
-    {
-        path: "/academic-faculty",
-        route: AcademicFacultyRoutes
-    },
-
-    {
-        path: "/academic-department",
-        route:AcademicDepartmentRouter
-    },
+ 
 ];
 
 modulesRoute.forEach(route => router.use(route.path, route.route))
